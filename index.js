49841dcb22e9c0e1eade13e5591e5269271fa6f0
CabosManuel <cabos.apolaya@gmail.com>
2025-03-10 16:33:15 -0500

    Endpoints

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/endpoint', (req, res) => {
+  res.send('Endpoint!');
+});
+
+app.get('/products', (req, res) => {
+  // Responder en formato JSON
+  res.json({
+    name: 'Product 1',
+    price: 100,
+  });
+});
+
 app.listen(port, () => {
   console.log('Port: ', port);
 });
