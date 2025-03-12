2820db3f7869892827cabfecaaf77270654451cc
CabosManuel <cabos.apolaya@gmail.com>
2025-03-12 13:59:00 -0500

    Single Responsibility with Routers

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { faker } from '@faker-js/faker';
+import routerApi from "./routes/index.js";
 
 const app = express();
 const port = 3000;
@@ -12,61 +12,8 @@ app.get('/endpoint', (req, res) => {
   res.send('Endpoint!');
 });
 
-app.get('/products', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-
-  for (let i = 0; i < limit; i++) {
-    products.push(
-      {
-        name: faker.commerce.productName(),
-        price: faker.commerce.price(),
-        image: faker.image.url(),
-      }
-    );
-  }
-
-  res.json(products);
-});
-
-
-app.get('/products/filter', (req, res) => {
-  res.send('Filter!');
-});
-
-app.get('/products/:productId', (req, res) => {
-  const { productId } = req.params;
-  res.json(
-    {
-      productId,
-    }
-  );
-});
-
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json(
-      {
-        limit,
-        offset,
-      }
-    );
-  } else {
-    res.send('No parameters');
-  }
-});
-
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json(
-    {
-      categoryId,
-      productId,
-    }
-  );
-});
+// Enviamos el app para que lo utilicen los routers
+routerApi(app);
 
 app.listen(port, () => {
   console.log('Port: ', port);
---
+++ routes/products.router.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import { faker } from '@faker-js/faker';
+
+// Router especifico para productos
+const router = express.Router();
+
+// Endpoints que van despues de /products:
+
+router.get('/', (req, res) => {
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+
+  for (let i = 0; i < limit; i++) {
+    products.push(
+      {
+        name: faker.commerce.productName(),
+        price: faker.commerce.price(),
+        image: faker.image.url(),
+      }
+    );
+  }
+
+  res.json(products);
+});
+
+
+router.get('/filter', (req, res) => {
+  res.send('Filter!');
+});
+
+router.get('/:productId', (req, res) => {
+  const { productId } = req.params;
+  res.json(
+    {
+      productId,
+    }
+  );
+});
+
+// Exportamos el router
+export default router;
