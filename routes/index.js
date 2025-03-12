e5da63c88f22a8656af202cb6ce360a7d9df988e
CabosManuel <cabos.apolaya@gmail.com>
2025-03-12 14:15:13 -0500

    Version Routing

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
+import express from 'express';
 import productRouter from './products.router.js';
 import usersRouter from './users.router.js';
 import categoriesRouter from './categories.router.js';
 
 function routerApi(app) {
-  app.use("/products", productRouter);
-  app.use("/users", usersRouter);
-  app.use("/categories", categoriesRouter);
+  const router = express.Router();
+  app.use("/api/v1", router);
+
+  router.use("/products", productRouter);
+  router.use("/users", usersRouter);
+  router.use("/categories", categoriesRouter);
 }
 
 export default routerApi;
