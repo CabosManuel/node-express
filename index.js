d0d70bf76437840aa81269d90c2703d277083ac6
CabosManuel <cabos.apolaya@gmail.com>
2025-03-16 16:31:05 -0500

    CORS

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 import express from 'express';
 import routerApi from "./routes/index.js";
 import { logErrors, errorHandler, boomErrorHandler } from "./middlewares/error.handler.js"
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
 // Middleware, para procesar solicitudes JSON
 app.use(express.json());
+
+// app.use(cors()); // Middleware para habilitar CORS a CUALQUIER dominio
+
+// Habilitar CORS a un dominio específico
+const whitelist = [ 'http://localhost:8080', 'https://otrodominio.com' ];
+const options = {
+  origin: (origin, callback) => {
+    if (!whitelist.includes(origin)) {
+      callback(new Error('Origen no permitido.'));
+    } else {
+      callback(null, true);
+    }
+  }
+}
+app.use(cors(options)); // Middleware para habilitar CORS a dominios específicos
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
